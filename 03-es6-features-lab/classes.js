2184e8ee92f4c51d9965ba996fb1b6a0b4137f93
Trayan Iliev <office@iproduct.org>
2022-07-27 14:34:31 +0300

    demos added

--- 03-es6-features-lab/classes.js
+++ 03-es6-features-lab/classes.js
@@ -6,14 +6,17 @@ const Role = ['READER', 'AUTHOR', 'ADMIN'];
 
 class Person {
     static nextId = 0;
-    id = ++ Person.nextId;
+    id = ++Person.nextId;
     constructor(fName, lName, adress) {
         this.fName = fName;
         this.lName = lName;
         this.adress = adress;
     }
+    getFullName() {
+        return `${this.fName} ${this.lName}`
+    }
     toString() {
-        return `ID: ${this.id}, Name: ${this.fName + ' ' + this.lName}, Address: ${this.adress}`;
+        return `ID: ${this.id}, Name: ${this.getFullName()}, Address: ${this.adress}`;
     }
 }
 
@@ -29,6 +32,26 @@ class User extends Person {
     }
 }
 
+
+const changePassword = function (newPassword) {
+    this.password =newPassword;
+}
+
+const SUPER_ADMIN = {
+    __proto__: User.prototype,
+    id: 0,
+    fName: 'Default',
+    lName: 'Admin',
+    adress: 'BG',
+    username: 'admin',
+    password: 'admin',
+    role: ADMIN,
+    toString() { // overriding
+        return `SUPERUSER: ${super.toString()}`;
+    },
+    changePassword
+}
+
 const p1 = new Person('John', 'Doe', 'London');
 const p2 = new Person('Jane', 'Doe', 'New York');
 const u1 = new User('Ivan', 'Petrov', 'Sofia 1000', 'ivan', 'ivan123')
@@ -36,3 +59,7 @@ const u2 = new User('Hristina', 'Petrova', 'Sofia 1000', 'hrisi', 'hrisi123', AD
 const u3 = new User('Georgi', 'Hristov', 'Plovdiv', 'gosho', 'gosho123', AUTHOR)
 const persons = [p1, p2, u1, u2, u3];
 persons.forEach(p => console.log(p.toString()))
+
+console.log(SUPER_ADMIN.toString());
+SUPER_ADMIN.changePassword('pass123');
+console.log(SUPER_ADMIN.toString());
