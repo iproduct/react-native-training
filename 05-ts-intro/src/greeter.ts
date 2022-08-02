c3c4aaf688cc3ab18078401cb0d4f1abb8845af8
Trayan Iliev <office@iproduct.org>
2022-08-02 14:43:50 +0300

    eslint config added

--- 05-ts-intro/src/greeter.ts
+++ 05-ts-intro/src/greeter.ts
@@ -2,9 +2,9 @@ function greeter(name: string): string {
     return `Hello ${name} from TypeScript!`;
 }
 
-// document.getElementById('results')!.innerHTML = greeter('Trayan');
+document.getElementById('results')!.innerHTML = greeter('Trayan');
 
-const elem = document.getElementById('results');
-if (elem != null) {
-    elem.innerHTML = greeter('Trayan');
-}
+// const elem = document.getElementById('results');
+// if (elem != null) {
+//     elem.innerHTML = greeter('Trayan');
+// }
