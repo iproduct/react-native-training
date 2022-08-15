66d62d1f3a5a8ac8a1bdffddb708562a71d11749
Trayan Iliev <office@iproduct.org>
2022-08-15 13:45:10 +0300

    react useEffect hook demo

--- 07-ts-react/src/index.tsx
+++ 07-ts-react/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TodoApp from './TodoApp';
 import reportWebVitals from './reportWebVitals';
+import TodoAppFunction from './TodoAppFunction';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <TodoApp />
+    <TodoAppFunction />
   </React.StrictMode>
 );
 
