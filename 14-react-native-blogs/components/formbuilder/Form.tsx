c66b730bbba82b774cb04bb5deb3f36f017d3a58
Trayan Iliev <office@iproduct.org>
2022-08-23 11:58:24 +0300

    Form component code cleanup

--- 14-react-native-blogs/components/formbuilder/Form.tsx
+++ 14-react-native-blogs/components/formbuilder/Form.tsx
@@ -5,6 +5,8 @@ import { StyleProp, View, ViewStyle, StyleSheet, ScrollView, SafeAreaView, Statu
 import IconButton from '../IconButton';
 import { FormTextComponent, FormTextComponentOptions } from './FormTextComponent';
 import { FormDropdownComponent, FormDropdownComponentOptions } from './FormDropdownComponent';
+import { FormComponentState } from './FormComponent';
+import { AnyConfiguration } from '@expo/webpack-config/webpack/types';
 
 
 
@@ -18,68 +20,13 @@ interface FormProps<Entity, FormConfig extends PropToComponentKindMapping<Entity
     style?: StyleProp<ViewStyle>;
 }
 
-interface FormState<Entity> {
-
+type FormState<Entity> = {
+    [Prop in keyof Entity]: FormComponentState<Entity[Prop]>
 }
 
 
-export class Form<Entity, FormConfig extends PropToComponentKindMapping<Entity>> extends Component<FormProps<Entity, FormConfig>, FormState<Entity>> {
-    // init() {
-    //     for (const elemId in this.intitialValue) {
-    //         this.elements[elemId].initialValue = this.intitialValue[elemId];
-    //     }
-    // }
-
-    // reset = () => {
-    //     for (const elemId in this.elements) {
-    //         this.elements[elemId]?.reset();
-    //     }
-    // }
-    // validate(): ValidationResult<Entity> {
-    //     const result: ValidationResult<Entity> = {};
-    //     this.valid = ValidStatus.VALID;
-    //     for (const elemId in this.elements) {
-    //         const fieldErrors = this.elements[elemId]?.validate();
-    //         if (fieldErrors && fieldErrors.length > 0) {
-    //             result[elemId] = fieldErrors;
-    //             this.valid = ValidStatus.INVALID;
-    //         }
-    //     }
-    //     return result;
-    // }
-
-    // getFormSnapshot(): Entity {
-    //     const result: Entity = { ...this.intitialValue };
-    //     for (const elemId in this.elements) {
-    //         const elemValue = this.elements[elemId].value;
-    //         if (elemId in this.elements) {
-    //             switch (typeof result[elemId]) {
-    //                 case 'string':
-    //                     result[elemId] = elemValue as unknown as Entity[Extract<keyof Entity, string>];
-    //                     break;
-    //                 case 'number':
-    //                     result[elemId] = (!Number.isNaN(parseFloat(elemValue)) ? parseFloat(elemValue) : undefined) as unknown as Entity[Extract<keyof Entity, string>];
-    //                     break;
-    //                 case 'boolean':
-    //                     result[elemId] = new Boolean(elemValue) as unknown as Entity[Extract<keyof Entity, string>];
-    //                     break;
-    //                 case 'undefined':
-    //                     result[elemId] = undefined as unknown as Entity[Extract<keyof Entity, string>];
-    //                     break;
-    //                 case 'object':
-    //                     if (Array.isArray(result[elemId])) {
-    //                         result[elemId] = elemValue.split(/\W+/) as unknown as Entity[Extract<keyof Entity, string>];
-    //                     } else {
-    //                         throw Error("Unexpected object field type when getting form snapshot")
-    //                     }
-    //                     break;
-    //                 default:
-    //                     throw Error("Unexpected field type when getting form snapshot")
-    //             }
-    //         }
-    //     }
-    //     return result;
-    // }
+export class Form<Entity, FormConfig extends PropToComponentKindMapping<Entity>> extends
+    Component<FormProps<Entity, FormConfig>, FormState<Entity>> {
 
     render() {
         const { config, style = {}, initialValue } = this.props;
@@ -117,77 +64,13 @@ export class Form<Entity, FormConfig extends PropToComponentKindMapping<Entity>>
                 </ScrollView>
             </SafeAreaView>
         );
-        // ` 
-        // <form id="${this.formId}" class="col s12">
-        //     ${fieldsMarkup}
-        //     <button class="btn waves-effect waves-light" type="submit" name="submit">Submit
-        //         <i class="material-icons right">send</i>
-        //     </button>
-        //     <button id="${this.formId}-reset-button" class="btn waves-effect waves-light red lighten-1" type="button">Reset
-        //         <i class="material-icons right">cached</i>
-        //     </button>
-        // </form>
-        // `;
     }
-
-    // makeInteractive() {
-    //     const formElem = document.getElementById(this.formId)! as HTMLFormElement;
-    //     formElem.addEventListener('submit', this.handleSubmit);
-    //     const resetButton = document.getElementById(`${this.formId}-reset-button`)! as HTMLButtonElement;
-    //     resetButton.addEventListener('click', this.reset);
-    //     // formElem.addEventListener('change', this.validateForm, true);
-
-    //     formElem.addEventListener('keyup', this.fieldChanged, true);
-
-    // }
-
-    // private fieldChanged = (event: KeyboardEvent) => {
-    //     const target = event.target as HTMLInputElement;
-    //     const value = (event.target as HTMLInputElement).value;
-    //     if (typeof value !== 'undefined') {
-    //         this.elements[target.getAttribute('data-property')! as keyof Entity].value = value as string;
-    //     }
-    // }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: StatusBar.currentHeight,
-    },
-    form: {
-        padding: 10,
-        width: '100%',
-    },
-    label: {
-        paddingTop: 10,
-        fontSize: 20,
-        alignSelf: 'flex-start',
-    },
-    input: {
-        fontSize: 24,
-        padding: 5,
-        borderColor: 'gray',
-        borderWidth: 1,
-    },
-    idField: {
-        fontSize: 24,
-        width: '100%',
-        height: 45,
-        padding: 5,
-        borderColor: 'gray',
-        borderWidth: 1,
-    },
-    dateChooser: {
-        display: 'flex',
-        flexDirection: 'row',
-    },
-    dateInput: {
-        flex: 1,
-        fontSize: 20,
-        padding: 5,
-        borderColor: 'gray',
-        borderWidth: 1,
     },
     buttons: {
         fontSize: 45,
@@ -197,10 +80,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         gap: 10,
         width: '100%',
-    },
-    buttonText: {
-        fontSize: 24,
-        color: 'white',
     },
 });
 
