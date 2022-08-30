30c34b38033ca54c35a851b8ddf8ab379101556d
Trayan Iliev <office@iproduct.org>
2022-08-30 11:45:31 +0300

    flat list infinite scrolling demo added

--- 15-react-native-apis-comps/Stagger.tsx
+++ 15-react-native-apis-comps/Stagger.tsx
@@ -1,59 +1,102 @@
 import React, { Component } from 'react'
-import { Animated, ScrollView, StyleSheet, Text } from 'react-native';
+import { Animated, FlatList, SafeAreaView, StyleSheet, Text, View, Dimensions } from 'react-native';
 
 const ITEM_WIDTH = 300;
+const PAGE_SIZE = 20;
 
-const arrValues: number[] = [];
-for (let i = 1; i < 500; i++) {
-    arrValues.push(i);
+type Item = string;
+
+interface StaggerState {
+    items: Item[];
+    page: number;
+    hasScrolled: boolean;
 }
 
-export default class Stagger extends Component {
+export default class Stagger extends Component<{}, StaggerState> {
+    state: Readonly<StaggerState> = {
+        items: [],
+        page: 0,
+        hasScrolled: false,
+    };
 
-    animatedValue = arrValues.map(val => new Animated.Value(0))
+    animatedValues: Animated.Value[] = [];
 
     componentDidMount(): void {
-        this.animate();
+        this.loadMoreItems();
     }
 
-    animate() {
-        const animations = arrValues.map((val, index) => Animated.timing(this.animatedValue[index], {
-            toValue: 1,
-            duration: 3000,
-            useNativeDriver: true,
-        }));
-        Animated.stagger(20, animations).start();
+    addAnimatedValues(newItems: Item[]) {
+        newItems.forEach(item => this.animatedValues.push(new Animated.Value(0)));
+        const newAnimations = newItems.map((val, index) =>
+            Animated.timing(this.animatedValues[index + this.state.page * PAGE_SIZE], {
+                toValue: 1,
+                duration: 500,
+                useNativeDriver: true,
+            }));
+        Animated.stagger(100, newAnimations).start();
+    }
+
+    addItems(newItems: Item[]) {
+        this.addAnimatedValues(newItems);
+        this.setState(({ items, page }) => ({ items: items.concat(newItems), page: page + 1 }));
+
+    }
+
+    loadMoreItems = () => {
+        if (this.state.page > 0 && !this.state.hasScrolled) return
+        const newItems: Item[] = [];
+        for (let i = this.state.page * PAGE_SIZE; i < (this.state.page + 1) * PAGE_SIZE; i++) {
+            newItems.push(`Item ${i}`);
+        }
+        this.addItems(newItems);
     }
 
     render() {
-        const animItems = arrValues.map((val, index) => (
-            <Animated.View key={index} style={[styles.item, {opacity: this.animatedValue[index]}]}>
-                <Text style={styles.text}>Item {val}</Text>
+        const animItems = this.state.items.map((val, index) => (
+            <Animated.View key={index} style={[styles.item, {
+                opacity: this.animatedValues[index],
+                marginLeft: this.animatedValues[index].interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [-ITEM_WIDTH, 0],
+                    extrapolate: 'clamp',
+                })
+            }]}>
+                <Text style={styles.text}>{val}</Text>
             </Animated.View>
         ));
         return (
-            <ScrollView contentContainerStyle={styles.container}>
-                {animItems}
-            </ScrollView>
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={animItems}
+                    renderItem={({ item }) => item}
+                    onEndReachedThreshold={0.1}
+                    onEndReached={this.loadMoreItems}
+                    onScroll={() =>
+                        this.setState({ hasScrolled: true })
+                    }
+                >
+                </FlatList>
+            </SafeAreaView>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        height: Dimensions.get('window').height,
+    },
     item: {
-      backgroundColor: '#ccc',
-      padding: 10,
-      borderWidth: 1,
-      borderColor: '#aaa',
-      borderRadius: 5,
-      width: ITEM_WIDTH,
-      textAlign: 'center',
-      marginLeft: 0,
-      marginTop: 3,
+        backgroundColor: '#ccc',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#aaa',
+        borderRadius: 5,
+        width: ITEM_WIDTH,
+        textAlign: 'center',
+        marginLeft: 0,
+        marginTop: 3,
     },
     text: {
         fontSize: 20,
     },
-    container: {
-    }
 });
