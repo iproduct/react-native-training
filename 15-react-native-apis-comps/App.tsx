ac2f7d58d2a64c2cdee51276eb6f602eb2efc9cf
Trayan Iliev <office@iproduct.org>
2022-08-31 14:13:57 +0300

    scrollview animated header

--- 15-react-native-apis-comps/App.tsx
+++ 15-react-native-apis-comps/App.tsx
@@ -1,7 +1,8 @@
 import AppAnimation01 from "./AppAnimation01";
 import AppAnimation02 from "./AppAnimation02";
 import AppEasing from "./AppEasing";
+import ScrollViewAnimatedHeader from "./ScrollViewAnimatedHeader";
 import Stagger from "./Stagger";
 
 export default () =>
-(<Stagger />)
+(<ScrollViewAnimatedHeader />)
