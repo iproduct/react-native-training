6b30a0c823f30e14c490bdfa079d741c523cf498
Trayan Iliev <office@iproduct.org>
2022-09-02 12:36:10 +0300

    drag and drop demo initial commit

--- 15-react-native-apis-comps/App.tsx
+++ 15-react-native-apis-comps/App.tsx
@@ -2,6 +2,7 @@ import { Dimensions, SafeAreaView, StyleSheet } from "react-native";
 import AppAnimation01 from "./AppAnimation01";
 import AppAnimation02 from "./AppAnimation02";
 import AppEasing from "./AppEasing";
+import Draggable from "./Draggable";
 import GestureResponder from "./GestureResponder";
 import LightBox, { SAMPLE_IMAGES } from "./LightBox";
 import ScrollViewAnimatedHeader from "./ScrollViewAnimatedHeader";
@@ -9,9 +10,10 @@ import Stagger from "./Stagger";
 
 export default () =>
 (
-    <SafeAreaView style={styles.container}>
-        <LightBox images={SAMPLE_IMAGES} height={400} width={800}/>
-    </SafeAreaView>
+    // <SafeAreaView style={styles.container}>
+    //     <LightBox images={SAMPLE_IMAGES} height={400} width={800}/>
+    // </SafeAreaView>
+    <Draggable />
 )
 
 const styles = StyleSheet.create({
---
+++ 15-react-native-apis-comps/Draggable.tsx
@@ -0,0 +1,126 @@
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import {
+    StyleSheet,
+    View,
+    PanResponder,
+    Animated,
+    PanResponderGestureState,
+    GestureResponderEvent,
+    Text,
+    SafeAreaView
+} from "react-native";
+
+let CIRCLE_RADIUS = 30;
+
+const INTIAL_POS = { x: 100, y: 300 }
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+interface DraggableProps {
+
+}
+
+interface DraggableState {
+    panState: Point | undefined;
+}
+
+export default class Draggable extends Component<DraggableProps, DraggableState> {
+    panValue = new Animated.ValueXY(INTIAL_POS);
+    state: Readonly<DraggableState> = {
+        panState: undefined,
+    };
+    // private _val: Point;
+    private panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (evt, gestureState) => true,
+        onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+        onMoveShouldSetPanResponder: (evt, gestureState) => true,
+        onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+        onPanResponderMove: (e: GestureResponderEvent, gestureState: PanResponderGestureState) => {
+            // console.log({...gestureState});
+            this.setState({ panState: { x: gestureState.moveX, y: gestureState.moveY } })
+            Animated.event([
+                { nativeEvent: { pageX: this.panValue.x, pageY: this.panValue.y } }, null
+            ], {
+                useNativeDriver: false,
+            })(e, gestureState);
+        },
+
+        onPanResponderRelease: (e: GestureResponderEvent, gestureState: PanResponderGestureState) => {
+            Animated.spring(this.panValue, {
+                toValue: INTIAL_POS,
+                friction: 5,
+                useNativeDriver: false,
+            }).start();
+        }
+    });
+
+    // componentWillMount() {
+    // Add a listener for the delta value change
+    // this._val = { x: 0, y: 0 }
+    // this.state.pan.addListener((value) => this._val = value);
+    // Initialize PanResponder with move handling
+    //     this.panResponder = 
+    // }
+
+    render() {
+        const panStyle = {
+            transform: [{
+                translateX: Animated.subtract(this.panValue.x, CIRCLE_RADIUS)
+            }, {
+                translateY: Animated.subtract(this.panValue.y, CIRCLE_RADIUS)
+            }]
+        }
+        return (
+            <View style={styles.conatainer}  >
+                <View style={styles.textConatainer} pointerEvents='none'>
+                    <Text>{this.state.panState?.x}, {this.state.panState?.y}</Text>
+                </View>
+                <View style={styles.dropZone} pointerEvents='none'>
+                    <Text style={styles.dropZoneText}>Drop them here!</Text>
+                </View>
+                <Animated.View
+                    {...this.panResponder.panHandlers}
+                    // onStartShouldSetResponder={() => true}
+                    // onMoveShouldSetResponder={() => true}
+                    // onResponderMove={(event) => {
+                    //     Animated.event([
+                    //         { nativeEvent: { pageX: this.panValue.x, pageY: this.panValue.y } }, null
+                    //     ], {
+                    //         useNativeDriver: false,
+                    //     })(event, null);
+                    // }}
+                    style={{ ...panStyle, ...styles.circle }}
+                />
+                <StatusBar hidden />
+            </View>
+        );
+    }
+}
+
+
+let styles = StyleSheet.create({
+    conatainer: {
+        flex: 1,
+    },
+    textConatainer: {
+        width: '100%',
+    },
+    circle: {
+        zIndex: 1,
+        backgroundColor: "skyblue",
+        width: CIRCLE_RADIUS * 2,
+        height: CIRCLE_RADIUS * 2,
+        borderRadius: CIRCLE_RADIUS
+    },
+    dropZone: {
+
+    },
+    dropZoneText: {
+
+    },
+});
