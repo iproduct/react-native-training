fb0db00dba6fb317c80dcf9272d129b22dd69a54
Trayan Iliev <office@iproduct.org>
2022-09-09 10:56:27 +0300

    old demo start

--- RN_Demo/index.js
+++ RN_Demo/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+import App from './src/App.old';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
